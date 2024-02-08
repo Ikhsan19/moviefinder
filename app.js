c5fd040aec5db046d4dd55ba895b8c2633527e70
Muhammad Ikhsan <muh.ikhsan818@gmail.com>
2024-02-08 16:56:44 +0800

    build: prepare project

---
+++ app.js
@@ -0,0 +1,12 @@
+// Import our custom CSS
+// import '../../scss/styles.scss'
+import './src/scss/styles.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// import 'regenerator-runtime';
+// import main from "./script/view/main.js";
+
+
+// document.addEventListener("DOMContentLoaded", main);
